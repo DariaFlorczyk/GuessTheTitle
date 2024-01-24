b05b162f3bc39fecf3807611aba28e64f2743c9a
Daria Florczyk <dariaflorczyk94@gmail.com>
2024-01-24 22:08:38 +0100

    ,,

--- src/Guess.java
+++ src/Guess.java
@@ -1,39 +1,39 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Guess {
     public static void main(String[] args) throws FileNotFoundException {
         try {
-        File file = new File("movies.txt");
-        Scanner scanner = new Scanner(file);
-
-        while (scanner.hasNextLine()) {
-            String line = scanner.nextLine();
-            System.out.println(line);
-        }
-        } catch(FileNotFoundException e) {
+            File file = new File("movies.txt");
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                System.out.println(line);
+            }
+        } catch (FileNotFoundException e) {
             System.out.println("File not found");
         }
-        String filmTitle =
+        String filmTitle = "the shawshank";
         String guesses = " "; //the user's guesses
         Scanner keyboard = new Scanner(System.in);
         boolean notDone = true;
         while (true) {
             notDone = false;
-            for (char filmTitle : filmTitle.toCharArray()) {
-                if (guesses.indexOf(filmTitle) == -1 {
-                    System.out.println('*');
+            for (char secretTitle : filmTitle.toCharArray()) {
+                if (guesses.indexOf(secretTitle) == -1) {
+                    System.out.print('*');
                     notDone = true;
-                }else{
-                    System.out.println(filmTitle);
+                } else {
+                    System.out.print(secretTitle);
                 }
             }
-            System.out.println("\nEnter your letter: ");
+            if (! notDone) {break;}
+            System.out.print("\nEnter your letter: ");
             String letter = keyboard.next();
             guesses += letter;
-                }
             }
+        System.out.println("\nCongratulations!");
         }
-}
-}
+    }
